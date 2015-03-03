51374379e5c863371f6b23c22ff316811b84d0df
Soren Hansen <Soren.Hansen@ril.com>
2015-03-03 22:03:02 +0100

    Create scripts to call vn_iface_* directly

--- setup.py
+++ setup.py
@@ -9,6 +9,8 @@ setuptools.setup(
         'console_scripts': [
             'netns-daemon-start = opencontrail_netns.daemon_start:daemon_start',
             'netns-daemon-stop = opencontrail_netns.daemon_stop:daemon_stop'
+            'vn-iface-create = opencontrail_netns.vn_iface_create:vn_iface_create',
+            'vn-iface-destroy = opencontrail_netns.vn_iface_destroy:vn_iface_destroy'
         ],
     }
     )
