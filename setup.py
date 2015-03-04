6256bcfc8294292650cadd82774db0d4b98bb808
Soren Hansen <Soren.Hansen@ril.com>
2015-03-04 14:16:01 +0100

    Fix syntax error

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
     entry_points = {
         'console_scripts': [
             'netns-daemon-start = opencontrail_netns.daemon_start:daemon_start',
-            'netns-daemon-stop = opencontrail_netns.daemon_stop:daemon_stop'
+            'netns-daemon-stop = opencontrail_netns.daemon_stop:daemon_stop',
             'vn-iface-create = opencontrail_netns.vn_iface_create:vn_iface_create',
             'vn-iface-destroy = opencontrail_netns.vn_iface_destroy:vn_iface_destroy'
         ],
